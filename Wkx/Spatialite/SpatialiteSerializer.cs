7ca62a53e5cb3d195ee8a8e878957a92e30d00ee
dunha <dmofhmr@gmail.com>
2022-08-16 17:33:56 +0800

    Add support for Spatialite binary geometry format

---
+++ Wkx/Spatialite/SpatialiteSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Wkx
+{
+    public class SpatialiteSerializer : IGeometrySerializer
+    {
+        public Geometry Deserialize(Stream stream)
+        {
+            return new SpatialiteReader(stream).Read();
+        }
+
+        public void Serialize(Geometry geometry, Stream stream)
+        {
+            byte[] buffer = new SpatialiteWriter().Write(geometry);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+}
