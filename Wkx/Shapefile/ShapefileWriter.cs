fa196aa86bb8e4402c151987c8f6cad5c99bc56a
dunha <dmofhmr@gmail.com>
2026-09-15 17:07:56 +0800

    Refactor excess usings

--- Wkx/Shapefile/ShapefileWriter.cs
+++ Wkx/Shapefile/ShapefileWriter.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
 using System.IO;
 using System.Linq;
-using System.Security.Principal;
-using System.Text;
 
 namespace Wkx
 {
